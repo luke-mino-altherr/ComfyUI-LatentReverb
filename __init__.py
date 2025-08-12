1b68d01db7672ab56b5d13d5fa3a779565aa4354
Luke Mino-Altherr <luke@comfy.org>
2025-08-12 09:58:25 -0700

    initial commit
    From cookiecutter repo

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for latent_reverb."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Luke Mino-Altherr"""
+__email__ = "luke@comfy.org"
+__version__ = "0.0.1"
+
+from .src.latent_reverb.nodes import NODE_CLASS_MAPPINGS
+from .src.latent_reverb.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
